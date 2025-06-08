952e723fa7c5ef2f83d9bb399c16c194433f5b6d
sivendar2 <sivendarp2023@gmail.com>
2025-06-08 14:54:39 -0400

    Update Main.tf

--- terraform/Main.tf
+++ terraform/Main.tf
@@ -29,6 +29,23 @@ resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
   role       = aws_iam_role.ecs_task_execution_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
+resource "aws_iam_policy" "codebuild_secrets_access" {
+  name        = "CodeBuildSecretsManagerAccess"
+  description = "Allow CodeBuild to read sonar-token-jenkins secret"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "secretsmanager:GetSecretValue"
+        ],
+        Resource = "arn:aws:secretsmanager:us-east-1:779846797240:secret:sonar_token*"
+      }
+    ]
+  })
+}
 
 resource "aws_cloudwatch_log_group" "ecs_log_group" {
   name              = var.cloudwatch_log_group_name
