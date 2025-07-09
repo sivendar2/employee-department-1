aa647b8c290f698f4df15eb1cfa3a8e6295b675f
sivendar <sivendarp2023@gmail.com>
2025-07-08 22:46:33 -0400

    vulunerable code

---
+++ src/main/java/com/ameya/service/VulnerableApp.java
@@ -0,0 +1,16 @@
+package com.ameya.service;
+
+import java.sql.*;
+
+public class VulnerableApp {
+    public void authenticateUser(String username) throws SQLException {
+        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/db", "user", "pass");
+        // Vulnerable: SQL Injection here
+        String sql = "SELECT * FROM users WHERE username = '" + username + "'";
+        Statement stmt = conn.createStatement();
+        ResultSet rs = stmt.executeQuery(sql);
+        if (rs.next()) {
+            System.out.println("User authenticated");
+        }
+    }
+}
